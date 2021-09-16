2edb242237281bb8cb1bf390373b262c0b401dff
nfluckiger20 <flu20002@byui.edu>
2021-09-16 11:13:36 -0600

    final bulletlist

--- lesson 01/script.js
+++ lesson 01/script.js
@@ -1,3 +1,9 @@
-alert('Hi there!');
+function modify_list(){
+    let newName=document.querySelector('#newName').value;
+    let li=document.createElement('li');
+    li.innerhtml=newName;
 
-console.log('Good morning!');
+    document.querySelector('#nameList').appendChild(li);
+}
+
+let new_li= document.getElementById('new_li', modify_list).innerHTML;
