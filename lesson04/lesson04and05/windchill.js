144f1a8d97d8e6b2663451507da513a3faabebb8
nfluckiger20 <flu20002@byui.edu>
2021-10-23 12:44:27 -0600

    container

---
+++ lesson04/lesson04and05/windchill.js
@@ -0,0 +1,9 @@
+console.log("Hello!")
+
+let currentTemp = document.getElementById("highTemp").innerHTML;
+let windSpeed = document.getElementById("windSpeed").innerHTML;
+let windChillTemp = document.getElementById("windChillTemp").innerHTML;
+
+windChillTemp = Math.floor(35.74 + (0.6215*currentTemp) - (35.75*Math.pow(windSpeed,0.16)) + (0.4275*currentTemp*Math.pow(windSpeed,0.16)))
+
+console.log(windChillTemp)
