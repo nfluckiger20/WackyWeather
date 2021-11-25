ee4a1411e2e9857d376f99acfbd48e3979e0aa15
nfluckiger20 <flu20002@byui.edu>
2021-11-24 23:54:13 -0700

    gallery

---
+++ WackyWeather/gallery/gallery.js
@@ -0,0 +1,35 @@
+let x=new Date()
+let y=x.getFullYear()
+document.getElementById("date").textContent=y
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -600px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entry) => {
+        if(!entry.isIntersecting){
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, imgOptions);
+
+images.forEach((image) => {
+    imgObserver.observe(image);
+});
+  
